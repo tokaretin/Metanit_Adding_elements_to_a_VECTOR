12abf81a95ebc6b64fa6eb7ba6fbac4755b0343d
tokaretin <tokaretin@gmail.com>
2023-05-26 18:28:58 +0200

    Добавьте файлы проекта.

---
+++ Metanit_Adding_elements_to_a_VECTOR/Metanit_Adding_elements_to_a_VECTOR.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+/*Для добавления элементов в вектор применяется функция push_back(), 
+в которую передается добавляемый элемент:*/
+
+int main()
+{
+    std::vector<int> vec; // создал пустой вектор
+    vec.push_back(1);
+    vec.push_back(34);
+    vec.push_back(46);
+    vec.push_back(46);
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << "Vec = " << vec[i] << ", "; // 1, 34, 46, 46
+    }
+    std::cout << '\n';
+
+    for (int elements : vec) {
+        std::cout << "Vec = " << elements << ", "; // 1, 34, 46, 46
+    }
+    std::cout << '\n';
+
+    std::cout << "vec.size() = " << vec.size() << '\n'; // всего элементов 4
+
+    /*Векторы являются динамическими структурами в отличие от массивов, 
+    где мы скованы его заданым размером. 
+    Поэтому мы можем динамически добавлять в вектор новые данные.*/
+
+    // Функция emplace_back() выполняет аналогичную задачу - добавляет элемент в конец контейнера:
+
+    std::vector<int> vecto{ 300, 400, 500, 600 };
+    vecto.emplace_back(555);
+
+    std::cout << "emplace_back() = ";
+    for (int n : vecto)
+    {
+        std::cout <<  n << ", "; // vecto { 300, 400, 500, 600, 555 }
+    }
+}
